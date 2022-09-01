b951a9a369f64294fe22e72e2f217e67595d3385
Matej Kondrot <kondrot.m@gmail.com>
2022-09-01 14:51:45 +0100

    JUnit testing

---
+++ src/test/java/com/sakila/api/SakilaApp/UnitTests/ActorTest.java
@@ -0,0 +1,49 @@
+package com.sakila.api.SakilaApp.UnitTests;
+
+import com.sakila.api.SakilaApp.Actor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ActorTest {
+
+    Actor a = new Actor(0,"Foo","Bar");
+
+    @Test
+    public void test_actor_initalise(){
+        Actor a = new Actor();
+    }
+    @Test
+    public void test_actor_full_init(){
+        Actor a = new Actor(1,"Nicki-James","Shepherd");
+    }
+    @Test
+    public void test_actor_get_id(){
+        Assertions.assertEquals(0,a.getActor_id());
+    }
+    @Test
+    public void test_actor_get_first_name(){
+        Assertions.assertEquals("Foo",a.getFirst_name());
+    }
+    @Test
+    public void test_actor_get_last_name(){
+        Assertions.assertEquals("Bar",a.getLast_name());
+    }
+    @Test
+    public void test_actor_set_first_name(){
+        Actor a = new Actor();
+        a.setFirst_name("Foo");
+        Assertions.assertEquals("Foo",a.getFirst_name());
+    }
+    @Test
+    public void test_actor_set_last_name(){
+        Actor a = new Actor();
+        a.setLast_name("Bar");
+        Assertions.assertEquals("Bar",a.getLast_name());
+    }
+    @Test
+    public void test_actor_set_id(){
+        Actor a = new Actor();
+        a.setActor_id(1);
+        Assertions.assertEquals(1,a.getActor_id());
+    }
+}
